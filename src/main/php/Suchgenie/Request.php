089c16ef4f3d731a17b6bb63095143e0e004f01a
David Sauer <david@nur-mal-so-am-ran.de>
2013-03-27 01:01:31 +0100

    fixed filters in navigation
    * moved from documentParams to defaultParams

--- src/main/php/Suchgenie/Request.php
+++ src/main/php/Suchgenie/Request.php
@@ -1,10 +1,10 @@
 <?php
 
 class Suchgenie_Request extends Suchgenie_Requester {
-    
+
     private $defaultParams = array();
     private $documentsParams = array();
-    
+
     function getNavigation ($attributes=array()) {
         // query,                               comparators, filters,           attributes
         $params = array();
@@ -12,7 +12,7 @@ class Suchgenie_Request extends Suchgenie_Requester {
         $params['attributes'] = implode(',', $attributes);
         return $this->getParallelGet("/api/navigation.json", $params);
     }
-    
+
     function getDocuments ($attributes=array()) {
         // query, documentsPerPage, pageNumber, comparators, filters, sortings, attributes
         $params = array();
@@ -29,69 +29,69 @@ class Suchgenie_Request extends Suchgenie_Requester {
         $params += $this->documentsParams;
         return $this->getParallelGet("/api/documentIdentifiers.json", $params);
     }
-    
+
     function setQuery($query) {
         $this->defaultParams['query'] = $query;
         return $this;
     }
-    
+
     function setDocumentsPerPage($documentsPerPage) {
         $this->documentsParams['documentsPerPage'] = $documentsPerPage;
         return $this;
     }
-    
+
     function setPageNumber($pageNumber) {
         $this->documentsParams['pageNumber'] = $pageNumber;
         return $this;
     }
-    
+
     function setComparator($attribute, $comparatorClass) {
         $this->defaultParams[$attribute . 'Comparator'] = $comparatorClass;
         return $this;
     }
-    
+
     function setSorting($attribute, $direction) {
         $this->documentsParams['sort' . $attribute] = $direction;
         return $this;
     }
-    
+
     function setFilter($attribute, $comparisonType, $value) {
         $filter = 'filter' . $attribute . $comparisonType;
-        $this->documentsParams[$filter] = $value;
+        $this->defaultParams[$filter] = $value;
         if ($value == null) {
-            unset($this->documentsParams[$filter]);
+            unset($this->defaultParams[$filter]);
         }
         return $this;
     }
-    
+
     function setEqualFilter($attribute, $value) {
         $this->setFilter($attribute, "eq", $value);
         return $this;
     }
-    
+
     function setNotEqualFilter($attribute, $value) {
         $this->setFilter($attribute, "ne", $value);
         return $this;
     }
-    
+
     function setGreaterEqualFilter($attribute, $value) {
         $this->setFilter($attribute, "ge", $value);
         return $this;
     }
-    
+
     function setGreaterThenFilter($attribute, $value) {
         $this->setFilter($attribute, "gt", $value);
         return $this;
     }
-    
+
     function setLessEqualFilter($attribute, $value) {
         $this->setFilter($attribute, "le", $value);
         return $this;
     }
-    
+
     function setLessThenFilter($attribute, $value) {
         $this->setFilter($attribute, "lt", $value);
         return $this;
     }
-    
+
 }
